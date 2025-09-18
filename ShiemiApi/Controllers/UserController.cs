c77c586a0e3a095a08466b648618837adabd2b78
douglas <boseallen192@gmail.com>
2025-09-18 14:06:58 +0530

    1. created user crud endpoints 2.user crud db 3.project crud db

--- ShiemiApi/Controllers/UserController.cs
+++ ShiemiApi/Controllers/UserController.cs
@@ -1,21 +1,64 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace ShiemiApi.Controllers{
+using ShiemiApi.Repositories;
+using ShiemiApi.Models;
 
-	[ApiController]
-	[Route("/api/[controller]")]
-	public class UserController{
-		
-		[HttpGet("/{Id}")] 
-		public IResult GetUser(int Id){
-			try{
-				
-				return Results.Ok();
-			}
-			catch(Exception ex){
-				Console.WriteLine("GetUser error: "+ex.Message);
-				return Results.BadRequest();
-			}
-		}
-	}
+namespace ShiemiApi.Controllers
+{
+
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class UserController(UserRepository userRepo)
+    {
+
+        private readonly UserRepository _userRepo = userRepo;
+
+        [HttpPost("/create")]
+        public IResult Create(User user)
+        {
+            try
+            {
+                _userRepo.Create(user);
+                return Results.Ok();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("CreateUser error: " + ex.Message);
+                return Results.BadRequest();
+            }
+        }
+
+        [HttpGet("/get/{Id}")]
+        public IResult Get(int Id)
+        {
+            try
+            {
+                var dbUser = _userRepo.GetById(Id);
+                if (dbUser is null)
+                    return Results.NotFound();
+
+                return Results.Ok(dbUser);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("GetUser error: " + ex.Message);
+                return Results.BadRequest();
+            }
+        }
+
+        [HttpPut("/update/{Id}")]
+        public IResult Update(int Id,User user)
+        {
+            try
+            {
+                _userRepo.Update(user);
+                return Results.Ok();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("UpdateUser error: " + ex.Message);
+                return Results.BadRequest();
+            }
+        }
+    }
 }
