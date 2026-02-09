b99911d64a9e2aff0e26e17bfeb882e38cb6e319
Allen Bose <boseallen192@gmail.com>
2026-02-09 22:40:07 +0530

    added space between client name

--- ShiemiApi/Controllers/RoomController.cs
+++ ShiemiApi/Controllers/RoomController.cs
@@ -121,7 +121,7 @@ public class RoomController(
                     RoomId = r.Id,
                     ClientId = r.Tenant.Id,
                     ProfilePhotoURL = r.Tenant.ProfilePhoto.URL,
-                    ClientName = r.Tenant.FirstName + r.Tenant.LastName
+                    ClientName = r.Tenant.FirstName+" "+ r.Tenant.LastName
                 };
                 devRoomDtos.Add(dto);
             }
